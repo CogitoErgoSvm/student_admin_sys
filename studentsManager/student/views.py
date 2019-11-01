d18cf47a647aa5d691444c2f878d09f325ed436d
huangwei01 <huangwei01@zhuanzhuan.com>
2019-11-01 18:24:00 +0800

    缩进修改

--- studentsManager/student/views.py
+++ studentsManager/student/views.py
@@ -10,24 +10,26 @@ def index(request):
     # words = 'Word!'
     # return render(request, 'index.html', context={'words': words})
     students = Student.objects.all()
+
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            cleaned_data = form.cleaned_data
-            student = Student()
-            student.name = cleaned_data['name']
-            student.sex = cleaned_data['sex']
-            student.email = cleaned_data['emaail']
-            student.profession = cleaned_data['profession']
-            student.qq = cleaned_data['qq']
-            student.phone = cleaned_data['phone']
-            student.save()
+            # cleaned_data = form.cleaned_data
+            #             # student = Student()
+            #             # student.name = cleaned_data['name']
+            #             # student.sex = cleaned_data['sex']
+            #             # student.email = cleaned_data['email']
+            #             # student.profession = cleaned_data['profession']
+            #             # student.qq = cleaned_data['qq']
+            #             # student.phone = cleaned_data['phone']
+            #             # student.save()
+            form.save()
             return HttpResponseRedirect(reverse('index'))
-        else:
-            form = StudentForm()
+    else:
+        form = StudentForm()
 
-        context = {
-            'students': students,
-            'form': form,
-        }
-        return render(request, 'index.html', context=context)
+    context = {
+        'students': students,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
